1719fc8ed2270c56ffc6ad4c7510d214f4f9739b
Ravikumar Badami <ravibadami191@gmail.com>
2026-03-21 23:50:01 +0530

    Form

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import Counter from './To-Do list/Hooks/Counter/index.jsx'
 import Toggle from './To-Do list/Hooks/Toggle/index.jsx'
+import Form from './To-Do list/Hooks/Form/index.jsx'
 
 function App() {
   return (
     <>
-      <Toggle/>
+      <Form/>
     </>
   )
 }
---
+++ learn-react/src/To-DO list/Hooks/Form/index.jsx
@@ -0,0 +1,32 @@
+    import {useState} from 'react';
+
+    export default function form(){
+
+    const [inputValue,setInputValue]=useState('');
+    const  [inputOnSubmit ,setInputOnSubmit ]=useState('');
+
+
+    const handleType=(e)=>{
+    setInputValue(e.target.value);
+      }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setInputOnSubmit(inputValue)
+        }
+
+
+
+
+
+        return(
+            <form onSubmit={handleSubmit}>
+              <input value={inputValue} onChange={handleType} />
+                <button onClick={handleSubmit}>Submit</button>
+                <p>{inputOnSubmit}</p>
+                </form>
+
+            );
+
+
+        }
